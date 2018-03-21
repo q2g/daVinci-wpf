8c2cbb1085da76347a188e92e862ca53a72b26a3
ChristofHoldschick <Christof.Holdschick@akquinet.de>
2018-03-21 05:10:51 +0100

    +Demo App +ColumnChooser +DimensionColumnData

---
+++ daVinci_Demo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using daVinci.Controls;
+using leonardo.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace daVinci_Demo
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ColumnChooser cc = new ColumnChooser();
+            cc.Columns.Add(new LuiAccordionItem() { Header = "TestDimension", Content = new DimensionColumnDataView() });
+            ItemList = new ObservableCollection<LuiAccordionItem>()
+            {
+                new LuiAccordionItem(){Header="Daten", Content=cc}
+            };
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public ObservableCollection<LuiAccordionItem> ItemList { get; set; }
+    }
+}
---
+++ src/Controls/ColumnChooser.xaml.cs
@@ -0,0 +1,35 @@
+﻿using leonardo.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace daVinci.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für ColumnChooser.xaml
+    /// </summary>
+    public partial class ColumnChooser : UserControl
+    {
+        public ColumnChooser()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        #region Accordion
+        public ObservableCollection<LuiAccordionItem> Columns { get; set; } = new ObservableCollection<LuiAccordionItem>();
+        #endregion
+    }
+}
