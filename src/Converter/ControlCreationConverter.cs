a0a53fc2fbd37f3ed064e69ecce05bed94f2233c
ChristofHoldschick <Christof.Holdschick@akquinet.de>
2018-03-26 16:51:02 +0200

    Controlcreation per Converter

---
+++ src/Converter/ControlCreationConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace daVinci.Converter
+{
+    [ValueConversion(typeof(object), typeof(object))]
+    public class ControlCreationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if(parameter is Type type)
+            {
+                var control = Activator.CreateInstance(type);
+                return control;
+            }
+            return null;
+            
+        }        
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {          
+            return null;
+        }
+
+       
+    }
+}
