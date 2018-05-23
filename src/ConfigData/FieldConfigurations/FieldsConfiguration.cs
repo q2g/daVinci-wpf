d93285e849dd320148ea065abd0eb42afeedd099
Christof Holdschik <christof.holdschick@akquinet.de>
2018-05-23 15:01:48 +0200

    * Add new Attached Property ItemHeight to adjust the Itemheight of an Accordionitem * begun Fields/MasterElement Configuration Dialog

---
+++ src/ConfigData/FieldConfigurations/FieldsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace daVinci.ConfigData.FieldConfigurations
+{
+    using leonardo.Resources;
+    #region Usings
+    using NLog;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    #endregion
+
+    public class FieldsConfiguration : INotifyPropertyChanged
+    {
+        #region LoggerInit
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+        #endregion
+
+        private ObservableCollection<DimensionMeasure> dimensionMeasures = new ObservableCollection<DimensionMeasure>();
+        public ObservableCollection<DimensionMeasure> DimensionMeasures
+        {
+            get
+            {
+                return dimensionMeasures;
+            }
+            set
+            {
+                dimensionMeasures = value;
+                RaisePropertyChanged();
+            }
+        }
+
+    }
+}
