01066d71648465a92774883238166e11528d2fda
Christof Holdschick <christof.holdschick@senseexcel.com>
2019-05-21 13:57:14 +0200

    start SettingsDialog

---
+++ src/ConfigData/Settings/SettingsItem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daVinci.ConfigData.Settings
+{
+    public class SettingsItem : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+        #endregion
+
+        #region Properties & variables
+        private string type;
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        private string itemValue;
+        public string ItemValue
+        {
+            get
+            {
+                return itemValue;
+            }
+            set
+            {
+                if (itemValue != value)
+                {
+                    itemValue = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ src/Controls/SettingsDialog.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace daVinci.Controls
+{
+    #region Usings
+    using daVinci.ConfigData.Connection;
+    using daVinci.ConfigData.Wizard;
+    using leonardo.Controls;
+    using leonardo.Resources;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Net;
+    using System.Net.Security;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Threading;
+    using WPFLocalizeExtension.Engine;
+    #endregion
+
+    /// <summary>
+    /// Interaktionslogik für SettingsDialog.xaml
+    /// </summary>
+    public partial class SettingsDialog : UserControl, INotifyPropertyChanged
+    {
+        Dispatcher currentDispatcher;
+        public SettingsDialog()
+        {
+            currentDispatcher = this.Dispatcher;
+            InitializeComponent();
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+        #endregion
+
+        #region Properties & Variables
+
+        //private ObservableCollection<> supportEmailCommand;
+        //public ICommand SupportEmailCommand
+        //{
+        //    get
+        //    {
+        //        return supportEmailCommand;
+        //    }
+        //    set
+        //    {
+        //        if (supportEmailCommand != value)
+        //        {
+        //            supportEmailCommand = value;
+        //            RaisePropertyChanged();
+        //        }
+        //    }
+        //}
+        private WizardItem currentStep;
+        public WizardItem CurrentStep
+        {
+            get
+            {
+                return currentStep;
+            }
+            set
+            {
+                if (currentStep != value)
+                {
+                    currentStep = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<ConnectionTestResult> Tests { get; set; } = new ObservableCollection<ConnectionTestResult>();
+        #endregion
+
+
+    }
+}
