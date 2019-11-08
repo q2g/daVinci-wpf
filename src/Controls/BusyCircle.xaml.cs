d22e7d19dae13323b85b71dbd83da579b491a3d8
Christof Holdschick <christof.holdschick@senseexcel.com>
2019-11-08 16:29:24 +0100

    busycircle new style

--- src/Controls/BusyCircle.xaml.cs
+++ src/Controls/BusyCircle.xaml.cs
@@ -25,5 +25,36 @@
         public static readonly DependencyProperty CirclethicknessProperty = DependencyProperty.Register(
          "Circlethickness", typeof(double), typeof(BusyCircle), new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
         #endregion
+
+
+
+        public Point StartPoint
+        {
+            get
+            {
+                return new Point(0, -1 * ActualHeight / 2);
+            }
+        }
+        public Point SpinnerPoint
+        {
+            get
+            {
+                return new Point(ActualHeight / 2, 0);
+            }
+        }
+        public double BackGroundRadius
+        {
+            get
+            {
+                return ActualHeight / 2;
+            }
+        }
+        public Size SpinnerSize
+        {
+            get
+            {
+                return new Size(ActualHeight / 2, ActualHeight / 2);
+            }
+        }
     }
 }
